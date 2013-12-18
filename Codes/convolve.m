198bbf0a3162650aa787a11050010b32481994bd
Tony Fast <tony.fast@gmail.com>
2013-12-18 15:26:55 -0500

    f2 is a clean, well documented function to compute spatial statistics with.  f2 is capable of handling computations on 1-D,2-D,3-D datasets with periodic, nonperiodic, and mixed boundary conditions.

--- Codes/convolve.m
+++ Codes/convolve.m
@@ -1,14 +1,17 @@
 function fA = convolve(period,A1,A2)
+% convolve pads the data signals then performs the convolution of signals
+% A1 and A2 using fast fourier transform algorithms.
 period_mult= .6; %describe this value
 
 % in reality you only need to pad to rmax
 realvals = isreal(A1)*ones(1,2);  % supresses imaginary parts if the values in the statistics
-nsz = ceil([period+period_mult].*size( A1)) ;
+nsz = ceil([ones(size(period)) + (1-period)*period_mult].*size( A1));
+
 
 fA = FourierPad( A1, size( A1 ), nsz );
 
 if exist('A2','var') && numel( A2 ) > 0
-    if all( nsz == ceil([period+period_mult].*size( A2)) );    % THIS CONDITION HERE IS PROBLABLY A PROBLEM
+    if all( nsz == ceil([ones(size(period)) + (1-period)*period_mult].*size( A2)) );    % THIS CONDITION HERE IS PROBLABLY A PROBLEM
         realvals(2) = isreal(A2);
         fA(:) = fA .* conj( FourierPad( A2, size(A1), nsz ));
     else
