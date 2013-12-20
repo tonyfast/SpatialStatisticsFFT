e7b3d7acc2fba386d35da202b0d9b9c2a5497e9f
Tony Fast <tony.fast@gmail.com>
2013-12-19 22:32:55 -0500

    Added a pair correlation computation function with an example included on its use.  Updated the examples page.

---
+++ PairCorrelation.m
@@ -0,0 +1,36 @@
+function [ Tpc Spc ] = PairCorrelation( A1, A2, varargin );
+% Pair correlation function after computing the spatial statistics.  There
+% is a faster way to do this using Alex Gray's methods.
+% 
+% Tpc - Is the average probability for the pair correlation
+% Spc - Is the standard deviation of the probability for the pair correlation
+
+switch nargin
+    case 1
+        [T,xx] = f2( A1, [],'display', false );
+    case 2
+        [T,xx] = f2( A1, A2,'display', false );
+    otherwise
+        str = '[T,xx] = f2( A1, A2, ';
+        for ii = 1 : numel( varargin)
+            str = horzcat( str, sprintf('varargin{%i},',ii));
+        end
+        str = horzcat(str(1:(end-1)),',''display'',false);');
+        eval(str);
+end
+
+XX = zeros(size(T));
+switch ndims( T )
+    case 1
+        XX(:) = round( xx.values{1} );
+    case 2
+        XX(:) = round(sqrt(bsxfun( @plus, [xx.values{1}.^2]', xx.values{2}.^2)));
+    case 3
+        XX(:) = round(sqrt(bsxfun( @plus, bsxfun( @plus, [xx.values{1}.^2]', xx.values{2}.^2),permute(xx.values{3},[ 1 3 2]))));
+end
+
+Tpc = accumarray( XX(:)+1, T(:),[], @mean);
+if nargout==2
+    Spc = accumarray( XX(:)+1, T(:),[], @std);
+end
+
