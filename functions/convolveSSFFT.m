b5374800c3f3f18adc851f740fee7dc2e7c81e6d
System Administrator <root@ipsec-143-215-22-113.vpn.gatech.edu>
2014-01-08 11:15:55 -0500

    Changed a lot of function names and deleted some extraneous stuff.

--- functions/convolveSSFFT.m
+++ functions/convolveSSFFT.m
@@ -8,12 +8,12 @@ realvals = isreal(A1)*ones(1,2);  % supresses imaginary parts if the values in t
 nsz = ceil([ones(size(period)) + (1-period)*period_mult].*size( A1));
 
 
-fA = FourierPad( A1, size( A1 ), nsz );
+fA = FourierPadSSFFT( A1, size( A1 ), nsz );
 
 if exist('A2','var') && numel( A2 ) > 0
     if all( nsz == ceil([ones(size(period)) + (1-period)*period_mult].*size( A2)) );    % THIS CONDITION HERE IS PROBLABLY A PROBLEM
         realvals(2) = isreal(A2);
-        fA(:) = fA .* conj( FourierPad( A2, size(A1), nsz ));
+        fA(:) = fA .* conj( FourierPadSSFFT( A2, size(A1), nsz ));
     else
         error('The sizes are incaptable.')
     end
