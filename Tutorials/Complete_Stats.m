bc4072f50181a9f05bb2dbd981d320cb5939959a
Tony Fast <tony.fast@gmail.com>
2014-05-09 13:54:49 -0400

    Moved tutorials to master

---
+++ Tutorials/Complete_Stats.m
@@ -0,0 +1,9 @@
+%% Populating the Complete Spatial Statistics
+% The material state features are digitized using digital signal processing
+% techniques and basis functions.  Each mode of the signal is a local state
+% and all of the signals compose the local state space.
+
+%% The Statistics Block Matrix
+% In another article, auto and cross correlations where discussed.
+%
+% $$\textbf{F}^{ij}$$ when $$i{\neq}j$$
---
+++ Tutorials/Symmetries.m
@@ -0,0 +1,1 @@
+%% Spatial Statistics and their Symmetries
