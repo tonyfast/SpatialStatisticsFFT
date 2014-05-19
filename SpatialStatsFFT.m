4fb3bec89f190ea29957bc24b8faade8f0889ebf
Tony Fast <tony.fast@gmail.com>
2014-05-19 16:24:18 -0400

    Updated Matlab functions on master

--- SpatialStatsFFT.m
+++ SpatialStatsFFT.m
@@ -366,4 +366,15 @@ fA(:) = ifftn( fA );
 
 if all(realvals) fA(:) = real(fA); end;
 
+
+end
+%%
+function varargout = getOutput(func,outputNo,varargin)
+% A function that selectively chooses the output of another function.
+% func - function handle
+% Outputnp - is the output index of interest
+% varargin - the inputs to the function
+    varargout = cell(max(outputNo),1);
+    [varargout{:}] = func(varargin{:});
+    varargout = varargout(outputNo);
 end
